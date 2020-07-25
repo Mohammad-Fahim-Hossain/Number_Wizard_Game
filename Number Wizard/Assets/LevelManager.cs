54d8eddee1b7f560bf245dfa8bacccf926433070
Mohammad Fahim Hossain <mohammad.fahim@northsouth.edu>
2020-07-25 14:19:31 +0600

    Upload the project

---
+++ Number Wizard/Assets/LevelManager.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelManager : MonoBehaviour {
+
+	// Use this for initialization
+	public void LoadLeve(string LevelNames){
+		Debug.Log ("Requested level" + LevelNames);
+		Application.LoadLevel (LevelNames);
+	}
+	public void QuitLevel(){
+		Debug.Log ("i want to Quit");	
+		Application.Quit ();
+	}
+}
---
+++ Number Wizard/Assets/NumberWizard.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class NumberWizard : MonoBehaviour {
+
+	int Max;
+	int Min;
+	int Guess;
+
+	public int MaxGuessAllow=10;
+	public  Text text;
+
+	// Use this for initialization
+	void Start () {
+		startgame ();
+	}
+	void startgame (){
+		Max = 1000;
+		Min = 1;
+		NextGuess ();
+	}
+	public void GuessHigher(){
+		Min = Guess;
+		NextGuess ();
+
+	}
+	public void GuessLower(){
+		Max = Guess;
+		NextGuess ();
+	}
+	void NextGuess(){
+		Guess = Random.Range (Min, Max + 1);
+		text.text = Guess.ToString ();
+		MaxGuessAllow = MaxGuessAllow - 1;
+		if (MaxGuessAllow <= 0) {
+			Application.LoadLevel ("win");
+		}
+	}
+
+
+	
+}
